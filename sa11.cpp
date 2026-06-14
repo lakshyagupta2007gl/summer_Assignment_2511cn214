4591635e4a54490721c2ae55f759b119dea67334
lakshyagupta2007gl <25114cn214@glbitm.ac.in>
2026-06-14 23:23:58 +0530

    Add files via upload

---
+++ sa11.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    int gcd;
+
+    for (int i = 1; i <= a && i <= b; i++) {
+        if (a % i == 0 && b % i == 0)
+            gcd = i;
+    }
+
+    cout << "GCD = " << gcd;
+
+    return 0;
+}
