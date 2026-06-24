859c9ca7b4184e9221e931f7916e0bb72c54b5e8
lakshyagupta2007gl <25114cn214@glbitm.ac.in>
2026-06-24 23:35:36 +0530

    DAY 17

---
+++ sa66.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n1, n2;
+    cin >> n1;
+    int a[n1];
+
+    for(int i = 0; i < n1; i++)
+        cin >> a[i];
+
+    cin >> n2;
+    int b[n2];
+
+    for(int i = 0; i < n2; i++)
+        cin >> b[i];
+
+    for(int i = 0; i < n1; i++)
+        cout << a[i] << " ";
+
+    for(int i = 0; i < n2; i++) {
+        bool found = false;
+
+        for(int j = 0; j < n1; j++) {
+            if(b[i] == a[j]) {
+                found = true;
+                break;
+            }
+        }
+
+        if(!found)
+            cout << b[i] << " ";
+    }
+
+    return 0;
+}
