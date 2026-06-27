bffeb0419591527e98cc21456ae467bacc314c93
lakshyagupta2007gl <25114cn214@glbitm.ac.in>
2026-06-28 00:45:03 +0530

    DAY 22

---
+++ sa87.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char str[100], ch;
+    int count = 0;
+
+    cout << "Enter a string: ";
+    cin.getline(str, 100);
+
+    cout << "nter char to find frequency: ";
+    cin >> ch;
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] == ch)
+            count++;
+    }
+
+    cout << "Frequency of '" << ch << "' = " << count;
+
+    return 0;
+}
